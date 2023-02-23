6d04c4c84e1934a19d273018f1e5ef79222856b4
vinaybarve <vinaybarve40@gmail.com>
2023-02-23 23:26:30 +0530

    adding day3

--- frontend/justby/src/Components/Cart.js
+++ frontend/justby/src/Components/Cart.js
@@ -21,32 +21,8 @@ const Cart = () => {
         </div>
         </div>
       </div>
-      <section className='main-cart-section'>
-      <div className='section-2'>
-      <h1>Gifts Cards</h1>
-      <p>just arrived</p>
-
-      </div>
-      <div className='cart-items'>
-        <div className='cart-items-container'>
-            <div className='items-info'>
-            <div className='products'>
-                <img src='https://img4.nbstatic.in/tr:w-500/61f3ec271e2731000bdc591b.jpg' alt='data-img' />
-                
-            </div>
-            <div className='title'>
-                <h1>Tata Play HD Box With Hindi Super Value SD Pack</h1>
-                <p>Valid on:All days</p>
-                <p>Timings:12 AM- 11.59PM</p>
-                <button>Details</button>
-            </div>
-            <div className='add-minus-quanity'>
-                
-            </div>
-
-            </div>
-        </div>
-      </div>
+      <section>
+        <h1>cart page</h1>
       </section>
     </>
   )
