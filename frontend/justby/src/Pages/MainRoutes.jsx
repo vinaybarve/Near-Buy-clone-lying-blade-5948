647eb1e83037d2b821642a6cfcad3dd4c6b9afdc
Mhtshm-mir <mir.mohtashim66@gmail.com>
2023-02-24 13:37:37 +0530

    navbar&footerCompleted

--- frontend/justby/src/Pages/MainRoutes.jsx
+++ frontend/justby/src/Pages/MainRoutes.jsx
@@ -1,20 +1,21 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import EditProduct from "./EditProduct";
-import Login from "./Login";
-import Products from "./Products";
-import Signup from "./Signup";
-import SingleProduct from "./SingleProduct";
-
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import EditProduct from "./EditProduct"
+import Login from "./Login"
+import Products from "./Products"
+import Signup from "./Signup"
+import SingleProduct from "./SingleProduct"
+import Dashboard from "../Pages/Dashboard"
 export default function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Products />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/products" element={<Products />} />
       <Route path="/products/:id" element={<SingleProduct />} />
       <Route path="/products/:id/edit" element={<EditProduct />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="*" element={<h3>Page Not Found</h3>} />
     </Routes>
-  );
+  )
 }
